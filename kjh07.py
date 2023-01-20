43ee98278f287ce121f845456f58b93e77a17423
jihyeonkim1776 <114978231+jihyeonkim1776@users.noreply.github.com>
2023-01-20 16:14:53 +0900

    Add files via upload

---
+++ kjh07.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+from matplotlib import font_manager, rc
+font = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
+rc('font', family=font)
+
+#plt.figure(figsize=(12,8))
+
+money = pd.read_csv("money_data7.csv")
+
+
+import streamlit as st
+
+option = st.selectbox(
+    'How would you like to choice year ?',
+    ('2020', '2021', '2022'))
+
+option2 = int(option)
+
+st.write('You selected:', option)
+
+money = money[:] [money['A_YEAR']== option2]
+
+fig, ax = plt.subplots(2,2, figsize=(12,8))
+
+plt.subplot(221)
+plt.plot(  list( money['A_MONTH'] ), list( money['A_RATE'] ), color='red' , marker='o'     ) 
+plt.xticks(tuple(money['A_MONTH']) )
+plt.title('America Rate')
+
+
+plt.subplot(222)
+plt.plot(  list( money['A_MONTH'] ), list( money['K_RATE'] ), color='blue' , marker='o'     ) 
+plt.xticks(tuple(money['A_MONTH']) )
+plt.title('Korea Rate')
+
+plt.subplot(223)
+plt.plot(  list( money['A_MONTH'] ), list( money['KOSPI'] ), color='green' , marker='o'     ) 
+plt.xticks(tuple(money['A_MONTH']) )
+plt.title('Kospi')
+
+plt.subplot(224)
+plt.plot(  list( money['A_MONTH'] ), list( money['HOUSE_PRICE'] ), color='yellow' , marker='o'     ) 
+plt.xticks(tuple(money['A_MONTH']) )
+plt.title('Housing Price')
+
+st.pyplot(fig)
